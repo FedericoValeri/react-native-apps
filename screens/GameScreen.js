e8a6e0cc1a87730bc2363b61f480bc1e0fc0fe01
Federico <federico.valeri@unicam.it>
2023-04-05 09:30:23 +0200

    SafeAreaView

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,7 +2,22 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 export default function GameScreen() {
-  return <Text>GameScreen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      <View>{/* LOG ROUNDS */}</View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
