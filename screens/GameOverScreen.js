a3c81697bf450462eb59c77dd1108aeb1d58353e
Federico <federico.valeri@unicam.it>
2023-04-05 13:05:42 +0200

    Styling image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 export default function GameOverScreen() {
   return (
-    <View>
-      <Text>Game is over!</Text>
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/images/success.png")}
+          style={styles.image}
+        />
+      </View>
+      <Text>Your phone needed ? rounds to guess the number ?.</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
